1ce2aef4453b4b3c6956822f7f2672d395250fb5
Bastian Inuk Christensen <bastian@inuk.blog>
2020-03-22 15:07:46 +0100

    Initial work on SQL connection from .java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import java.sql.*;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("ugghhhh");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What's the students id you want grade average on? ");
+        int studentID = scanner.nextInt();
+
+        Connection conn;
+        String sqlite = "jdbc:sqlite:database/pofolio2database.dbsqlite";
+
+        try {
+            conn = DriverManager.getConnection(sqlite);
+            Statement stmt = conn.createStatement();
+            String query = "SELECT * FROM Student where ID ="+studentID;
+            ResultSet res = stmt.executeQuery(query);
+            System.out.println(res.getString("Name") + " got " + res.getInt("grade") + " in " + res.getString("Course"));
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
