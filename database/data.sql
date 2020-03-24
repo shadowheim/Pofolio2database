86aeebf30b61c6b76e81d040bde580cce2ab910a
Bastian Inuk Christensen <bastian@inuk.blog>
2020-03-24 10:03:01 +0100

    Added courses

--- database/data.sql
+++ database/data.sql
@@ -15,29 +15,33 @@ CREATE table IF NOT EXISTS Grades
     InstructorID INTEGER,
     Grade        INTEGER,
     foreign key (CourseID) REFERENCES Course (CourseID),
-    foreign key (StudentID) REFERENCES Students (StudentID)
+    foreign key (StudentID) REFERENCES Students (StudentID),
+    foreign key (InstructorID) REFERENCES Instructor (InstructorID)
 );
+
+CREATE TABLE IF NOT EXISTS Instructor (
+    InstructorID INTEGER PRIMARY KEY autoincrement,
+    Name VARCHAR
+);
+
 CREATE table IF NOT EXISTS Course (
     CourseID INTEGER primary key autoincrement,
     InstructorID INTEGER,
-    Name VARCHAR
-    FOREIGN KEY (Instructor) REFERENCES
-);
-CREATE TABLE IF NOT EXISTS Instructor (
-    InstructorID INTEGER PRIMARY KEY autoincrement,
     Name VARCHAR,
+    foreign key (InstructorID) REFERENCES Instructor (InstructorID)
 );
+
 INSERT INTO Students (Name, City) values
-    ('Aisha Lincoln', 'Nykøbing F'),
-    ('Anya Nielsen', 'Nykøbing F'),
-    ('Benjamin Jensen' , 'Camas'),
-    ('Berta Bertelsen' , 'Billund'),
-    ('Albert Antonsen','Sorø'),
-    ('Eske Eriksen','Eskildstrup'),
-    ('Olaf Olesen','Odense'),
-    ('Salma Simonsen','Stockholm'),
-    ('Theis Thomasen','Tølløse'),
-    ('Janet Jensen','Jyllinge')
+('Aisha Lincoln', 'Nykøbing F'),
+('Anya Nielsen', 'Nykøbing F'),
+('Benjamin Jensen' , 'Camas'),
+('Berta Bertelsen' , 'Billund'),
+('Albert Antonsen','Sorø'),
+('Eske Eriksen','Eskildstrup'),
+('Olaf Olesen','Odense'),
+('Salma Simonsen','Stockholm'),
+('Theis Thomasen','Tølløse'),
+('Janet Jensen','Jyllinge');
 
 insert into Instructor (Name) values ('Line'), ('Ebbe');
 
@@ -45,6 +49,4 @@ INSERT INTO Course (
     NAME,
     InstructorID
 ) values
-(SD 2019 autumn, 0),(SD 2020 spring, 0),(ES1 2019 autumn, 1);
-
-INSERT INTO Grades (StudentID, CourseID, InstructorID, Grade);
+('SD 2019 autumn', 1),('SD 2020 spring', 1),('ES1 2019 autumn', 2);
