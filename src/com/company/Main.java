41f61b8fdc5aaefa712f33cc44924625435a580b
Bastian Inuk Christensen <bastian@inuk.blog>
2020-03-24 10:57:37 +0100

    Added avg for a given student

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,17 +6,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("What's the students id you want grade average on? ");
-        int studentID = scanner.nextInt();
+     //   Scanner scanner = new Scanner(System.in);
+     //   System.out.println("What's the students id you want grade average on? ");
+        int studentID =1;// scanner.nextInt();
 
-        Connection cnct;
+        Connection cnct = null;
         String sqlite = "jdbc:sqlite:database/pofolio2database.dbsqlite";
 
         try {
             cnct = DriverManager.getConnection(sqlite);
             Statement stmt = cnct.createStatement();
-            String query = "SELECT * FROM Student where ID ="+studentID;
+            String query = "SELECT AVG(Grade) FROM Grades where StudentID ="+studentID;
             ResultSet res = stmt.executeQuery(query);
             System.out.println(res.getString("Name") + " got " + res.getInt("grade") + " in " + res.getString("Course"));
         } catch(SQLException e) {
