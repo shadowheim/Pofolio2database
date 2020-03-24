b541c640984409c8d6aa4fa8399b2ba0dce16e9c
Bastian Inuk Christensen <bastian@inuk.blog>
2020-03-24 11:35:31 +0100

    Added check for type

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,5 @@
 package com.company;
 
-import javax.imageio.IIOException;
 import java.io.IOException;
 import java.sql.*;
 import java.util.Scanner;
@@ -8,20 +7,26 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("What's the students id you want grade average on? ");
+        System.out.printf("Want to check students average? (y/n) ");
         try {
             char type = (char) System.in.read();
+            switch (type) {
+                case 'y':
+                case 'n':
+                    System.out.printf("Select the ID of what you wanted printed: ");
+                    Scanner scanner = new Scanner(System.in);
+                    int selectedID = scanner.nextInt();
 
-            System.out.printf("Select the ID of what you wanted printed: ");
-            Scanner scanner = new Scanner(System.in);
-            int selectedID = scanner.nextInt();
+                    Connection cnct = null;
+                    String sqlite = "jdbc:sqlite:database/pofolio2database.dbsqlite";
 
-            Connection cnct = null;
-            String sqlite = "jdbc:sqlite:database/pofolio2database.dbsqlite";
-
-            cnct = DriverManager.getConnection(sqlite);
-            Statement stmt = cnct.createStatement();
-            System.out.println("The sudent has an average of " + avgGrade(selectedID, stmt,GradeType.fromInt(type)));
+                    cnct = DriverManager.getConnection(sqlite);
+                    Statement stmt = cnct.createStatement();
+                    System.out.println("The sudent has an average of " + avgGrade(selectedID, stmt,GradeType.fromInt(type)));
+                    break;
+                default:
+                    System.out.printf("This is going to be weird!");
+            }
         }catch(IOException e) {
             System.out.println("I asked you to pick if you wanted the grade or the student");
         } catch(SQLException e) {
